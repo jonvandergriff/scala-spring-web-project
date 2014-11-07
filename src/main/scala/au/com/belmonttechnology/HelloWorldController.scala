dba23bcf2409747b1e879fba27bb21c8e1760ee5
JV018685 <jonathon.vandergriff@cerner.com>
2014-11-06 21:44:09 -0800

    this is awesome

--- src/main/scala/au/com/belmonttechnology/HelloWorldController.scala
+++ src/main/scala/au/com/belmonttechnology/HelloWorldController.scala
@@ -2,10 +2,31 @@ package au.com.belmonttechnology
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class HelloWorldController {
 
-  @RequestMapping(Array("/hello.html"))
-  def showHello = "helloPage"
+	@RequestMapping(Array("/hello.html"))
+	def showHello = "helloPage"
+
+	/**
+	 * This would be ideal for returning JSON content
+	 */
+	@RequestMapping(Array("/hi"))
+	@ResponseBody
+	def sayStuff: String = {
+		"this is me saying something"
+	}
+
+	/**
+	 * This would be ideal for returning JSON content
+	 */
+	@RequestMapping(Array("/name/{firstName}"))
+	@ResponseBody
+	def sayStuffWithParams(@PathVariable("firstName") firstName: String): String = {
+		"this is me saying something to" + firstName
+	}
+
 }
